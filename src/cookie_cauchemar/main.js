a77f7400abdb840443a8cbb4b28b27435a527d42
0bou0 <alexis.canale@etu.univ-smb.fr>
2024-12-05 23:01:23 +0100

    ajout du canvas

--- src/cookie_cauchemar/main.js
+++ src/cookie_cauchemar/main.js
@@ -1,19 +1,37 @@
-function Counter(element, compte) {
-  let counter = 0;
 
-  const setCounter = (count) => {
-    counter = count;
-    compte.innerHTML = `Points:  ${counter}`; 
-  };
 
-  element.addEventListener('click', () => setCounter(counter + 1));
-  setCounter(0);
-}
 
-const poisson = document.getElementById('poisson');
-const cpt = document.getElementById('cpt');
+onload = _ => {
 
-if (poisson) {
-  Counter(poisson, cpt);
+
+
+  let canvas = document.getElementById('jeu');
+  var ctx = canvas.getContext('2d');
+  ctx.globalCompositeOperation = 'destination-over'
+  ctx.fillStyle = 'lightblue';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'red';
+  ctx.fillRect(200, 200, 150, 100);
+  ctx.fill();
+
+
+  function Counter(element, compte) {
+    let counter = 0;
+  
+    const setCounter = (count) => {
+      counter = count;
+      compte.innerHTML = counter; 
+    };
+  
+    element.addEventListener('click', () => setCounter(counter + 1));
+    setCounter(0);
+  }
+  
+  const poisson = document.getElementById('jeu');
+  const cpt = document.getElementById('cpt');
+  
+  if (poisson && cpt) {
+    Counter(poisson, cpt);
+  }
 }
 

9cb3f2a66016e7f9ce5a462a1b691e930ef922b1
NoaLeDev <NoaLeDev@bg.com>
2024-12-06 01:38:00 +0100

    Ajout estomac + mise a jour contenu des modales

--- src/lightbox.js
+++ src/lightbox.js
@@ -1,11 +1,27 @@
-var modal = document.getElementById("ModalPoumon");
+var modal = document.getElementById("modal-id");
 
-var btn = document.getElementById("poumon");
+var poumon = document.getElementById("poumon");
+var coeur = document.getElementById("coeur");
+var estomac = document.getElementById("estomac");
 
 var span = document.querySelector(".close");
+var text = document.querySelector(".modal-text");
 
-btn.onclick = function () {
+
+
+poumon.onclick = function () {
     modal.style.display = "block";
+    text.innerHTML = "Les poumons sont un parallèles à la respiration de la flore marine..."
+}
+
+coeur.onclick = function () {
+    modal.style.display = "block";
+    text.innerHTML = "Le coeur gère le flux sanguin et le flux sanguin est lié aux courants marins de l'océan."
+}
+
+estomac.onclick = function () {
+    modal.style.display = "block";
+    text.innerHTML = "L'estomac est représenté par le plancton qui est à l'origine de la chaîne alimentaire de la faune marine."
 }
 
 span.onclick = function () {

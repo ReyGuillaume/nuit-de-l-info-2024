21e1836fc375fd730f795bed4d3274e0412d8fae
0bou0 <alexis.canale@etu.univ-smb.fr>
2024-12-06 07:09:24 +0100

    création du mode facile

--- src/clicker.js
+++ src/clicker.js
@@ -14,7 +14,9 @@ onload = _ => {
   console.log(canvas.width)
   console.log(canvas.height)
 
-  
+  let phi = 0;
+  let modeFacile = document.getElementById('mode-facile');
+  console.log(modeFacile)
 
   let taillePoissonClown = {
     x: 120,
@@ -53,6 +55,12 @@ onload = _ => {
   let flipClown = true;
   var loop = () => {
 
+    if (modeFacile.checked) {
+      time = 0.0005
+    }
+    else 
+      time = 0.001
+
     deltaTime = -currentTime + (currentTime = performance.now());
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -64,21 +72,19 @@ onload = _ => {
     const clownFlip = document.getElementById("clown-flip");
     const blob = document.getElementById("blob");
 
-    positionPoissonClownX = canvas.width/2 + (canvas.width/2 + taillePoissonClown.x)*Math.sin(currentTime * 0.001) - taillePoissonClown.x/2;
-    if (positionPoissonClownX <= -taillePoissonClown.x)
-      {
+    positionPoissonClownX = canvas.width/2 + (canvas.width/2 + taillePoissonClown.x)*Math.sin(currentTime * time) - taillePoissonClown.x/2;
+    if (positionPoissonClownX <= -taillePoissonClown.x) {
         flipClown = true;
       }
     if (positionPoissonClownX >= canvas.width) {
         flipClown = false;
     }
-    positionPoissonClownY = canvas.height/2 + 100*Math.sin(currentTime * 0.005);
+    positionPoissonClownY = canvas.height/2 + 100*Math.sin(currentTime * time*5);
 
     if (positionPoissonBlobY > canvas.height)
       positionPoissonBlobX = Math.random() * (canvas.width - taillePoissonBlob.x);
-    positionPoissonBlobY = canvas.height + 100*(Math.sin(currentTime * 0.005) - Math.abs(Math.sin(currentTime*0.005)) + 1);
+    positionPoissonBlobY = canvas.height + 100*(Math.sin(currentTime * time*5) - Math.abs(Math.sin(currentTime*time*5)) + 1);
     
-  console.log(flipClown)
     ctx.drawImage(flipClown ? clownFlip : clown, imagePoissonClown.x, imagePoissonClown.y, imagePoissonClown.width, imagePoissonClown.height, positionPoissonClownX, positionPoissonClownY, taillePoissonClown.x, taillePoissonClown.y);
 
     ctx.drawImage(blob, imagePoissonBlob.x, imagePoissonBlob.y, imagePoissonBlob.width, imagePoissonBlob.height, positionPoissonBlobX, positionPoissonBlobY, taillePoissonBlob.x, taillePoissonBlob.y);
@@ -90,7 +96,6 @@ onload = _ => {
 
   let counter = 0;
   canvas.addEventListener('click', (event) => {
-    console.log(getMousePos(canvas, event))
     let mousePos = getMousePos(canvas, event);
     if (
       mousePos.x >= positionPoissonClownX && 
@@ -98,16 +103,22 @@ onload = _ => {
       mousePos.x <= positionPoissonClownX + taillePoissonClown.x && 
       mousePos.y <= positionPoissonClownY + taillePoissonClown.y
     )
-      setCounter(1)
+      setCounter(1);
     else if (
       mousePos.x >= positionPoissonBlobX && 
       mousePos.y >= positionPoissonBlobY && 
       mousePos.x <= positionPoissonBlobX + taillePoissonBlob.x && 
       mousePos.y <= positionPoissonBlobY + taillePoissonBlob.y
     )
-      setCounter(5)
-    else
-      setCounter(-1)
+      setCounter(5);
+    else {
+      if (modeFacile.checked){
+        setCounter(-1);
+      }
+      else {
+        setCounter(-10);
+      }
+    }
   });
 
   function getMousePos(canvas, evt) {
@@ -125,7 +136,6 @@ onload = _ => {
     counter += number;
     if (counter < 0)
       counter = 0;
-    cpt.innerHTML = counter; 
   };
   
 }

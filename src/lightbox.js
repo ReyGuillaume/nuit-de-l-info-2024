586c1455851d71bcdcd11f5f9d9da027bdd69d83
NoaLeDev <NoaLeDev@bg.com>
2024-12-06 07:08:06 +0100

    Images avec organes

--- src/lightbox.js
+++ src/lightbox.js
@@ -22,25 +22,25 @@ poumon.onclick = function () {
 coeur.onclick = function () {
     modal.style.display = "block";
     text.innerHTML = "Le cœur humain est le moteur du corps, pompant le sang à travers un réseau de <a href='https://fr.vikidia.org/wiki/Vaisseau_sanguin' class='helper' target='_blank'>vaisseaux</a> pour transporter <a href='https://fr.vikidia.org/wiki/Oxyg%C3%A8ne' class='helper' target='_blank'>l’oxygène</a> et les <a href='https://fr.vikidia.org/wiki/Nutriment' class='helper' target='_blank'>nutriments</a> jusqu’aux organes. De manière semblable, les courants océaniques agissent comme le <a href='https://fr.vikidia.org/wiki/Syst%C3%A8me_circulatoire' class='helper' target='_blank'>système circulatoire</a> de la planète, déplaçant l’eau, la chaleur et les nutriments à travers les mers pour maintenir l’équilibre <a href='https://fr.vikidia.org/wiki/Climat' class='helper' target='_blank'>climatique</a> et nourrir les <a href='https://fr.vikidia.org/wiki/%C3%89cosyst%C3%A8me' class='helper' target='_blank'>écosystèmes</a> marins. Comme le sang qui relie chaque partie du corps, ces courants connectent les océans, influençant les <a href='https://fr.vikidia.org/wiki/Cycle_de_vie' class='helper' target='_blank'>cycles de vie</a> et régulant la température globale. Les perturbations climatiques, à l’image des <a href='https://fr.vikidia.org/wiki/Maladie_cardio-vasculaire' class='helper' target='_blank'>maladies cardiovasculaires</a> pour le corps, mettent en danger ce système vital. Préserver les courants océaniques, c’est protéger le battement régulier du cœur de la Terre."
-    img.src = ""
+    img.src = "https://img.freepik.com/photos-gratuite/belle-photo-vagues-mer_58702-11075.jpg?t=st=1733457546~exp=1733461146~hmac=d01e0349a811999f0cf6f1e446b4c6f610f0d99a9476d11dc1ccee244b72269a&w=740"
 }
 
 estomac.onclick = function () {
     modal.style.display = "block";
     text.innerHTML = "L’estomac transforme la nourriture en énergie pour le corps. L’océan, lui, agit comme un immense estomac pour la planète : il recycle les <a href='https://fr.vikidia.org/wiki/Nutriment' class='helper' target='_blank'>nutriments</a> et les déchets pour nourrir la vie marine. Tout comme les <a href='https://fr.vikidia.org/wiki/Acide' class='helper' target='_blank'>acides</a> de l’estomac décomposent les aliments, des organismes dans l’océan, comme le <a href='https://fr.vikidia.org/wiki/Plancton' class='helper' target='_blank'>phytoplancton</a>, transforment la matière en oxygène et en énergie. Mais si l’estomac peut être perturbé par des <a href='https://fr.vikidia.org/wiki/Toxine' class='helper' target='_blank'>toxines</a>, la pollution empêche l’océan de bien remplir son rôle, menaçant la vie sur Terre."
-    img.src = ""
+    img.src = "https://s3.animalia.bio/animals/photos/full/1x1/1200px-bottlenose-dolphinjpg.webp?id=d83b7573c8adc55da2c8665fdbe41d38"
 }
 
 cerveau.onclick = function () {
     modal.style.display = "block";
     text.innerHTML = "Le cerveau est comme un <a href='https://fr.vikidia.org/wiki/Chef_d%27orchestre' class='helper' target='_blank'>chef d’orchestre</a> : il dirige tout dans le corps, en contrôlant les mouvements, la respiration et les pensées. L’océan joue un rôle semblable pour la planète. Grâce à ses courants et à ses échanges, il régule le <a href='https://fr.vikidia.org/wiki/Climat' class='helper' target='_blank'>climat</a>, influence la météo et nourrit la vie. Comme le cerveau enregistre des souvenirs, l’océan garde la chaleur et le <a href='https://fr.vikidia.org/wiki/Carbone' class='helper' target='_blank'>carbone</a> pour aider la Terre à rester stable. Mais quand il est pollué ou trop chauffé, cet équilibre est perturbé, un peu comme quand le stress affecte le cerveau. Protéger l’océan, c’est veiller sur le cœur et l’esprit de notre planète."
-    img.src = ""
+    img.src = "https://img.freepik.com/photos-gratuite/gros-plan-thermometre-indiquant-temperature-elevee_23-2149456775.jpg?t=st=1733457839~exp=1733461439~hmac=e2bef89a4f5f871bf85869bbae4fef664a86301b2d979054bdbfe49ce5d3881d&w=740"
 }
 
 articulations.onclick = function () {
     modal.style.display = "block";
     text.innerHTML = "Les <a href='https://fr.vikidia.org/wiki/Articulation' class='helper' target='_blank'>articulations</a> relient les os et permettent au corps de bouger avec fluidité. Elles sont comme des charnières qui supportent et coordonnent tous les gestes. L’océan joue un rôle similaire pour la planète : ses courants et ses marées connectent les continents, transportent la chaleur et les <a href='https://fr.vikidia.org/wiki/Nutriment' class='helper' target='_blank'>nutriments</a>, et maintiennent le mouvement des <a href='https://fr.vikidia.org/wiki/%C3%89cosyst%C3%A8me' class='helper' target='_blank'>écosystèmes</a>. Quand les articulations sont enflammées ou abîmées, le corps devient raide et perd sa mobilité. De la même manière, si l’océan est pollué ou perturbé par le réchauffement, ses courants ralentissent ou changent de direction, affectant le <a href='https://fr.vikidia.org/wiki/Climat' class='helper' target='_blank'>climat</a> et la vie marine. Prendre soin de l’océan, c’est préserver la souplesse et <a href='https://fr.vikidia.org/wiki/Harmonie' class='helper' target='_blank'>l’harmonie</a> des mouvements de la planète."
-    img.src = ""
+    img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFMtDEVoRG6LnZkWbJ-wbi2MyPjW7mKrs_tA&s"
 }
 
 span.onclick = function () {

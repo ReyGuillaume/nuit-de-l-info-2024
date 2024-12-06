55359dd05daf56c7cc317ffb973b7fb774657ce7
NoaLeDev <NoaLeDev@bg.com>
2024-12-06 01:57:14 +0100

    poumon modal

--- src/lightbox.js
+++ src/lightbox.js
@@ -11,7 +11,7 @@ var text = document.querySelector(".modal-text");
 
 poumon.onclick = function () {
     modal.style.display = "block";
-    text.innerHTML = "Les poumons sont un parallèles à la respiration de la flore marine..."
+    text.innerHTML = 'Les poumons humains absorbent l’oxygène pour alimenter le corps en énergie et rejettent du CO₂. De manière similaire, l’océan joue un rôle clé dans la respiration de la planète grâce au phytoplancton, de minuscules organismes capables de transformer le CO₂ en oxygène grâce à la lumière du soleil. Ces "poumons bleus" produisent plus de la moitié de l’oxygène terrestre, tout en absorbant une partie du CO₂. Cet équilibre est fragile : la pollution et le réchauffement climatique perturbent cette respiration vitale. Protéger l’océan revient à préserver le souffle de la planète entière.'
 }
 
 coeur.onclick = function () {
